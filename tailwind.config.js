754dfd79d0cbf700c8a0e48aa452bcf045a600bd
Kion87 <ca.ion87@gmail.com>
2023-02-20 19:26:13 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html, js}", "./build/pages/*.{html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
